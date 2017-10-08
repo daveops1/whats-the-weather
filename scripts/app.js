eebcc44463616edb29d3b389a0803f32f2af39e7
David Horton <d.r.horton@outlook.com>
2017-10-08 04:19:54 +0000

    Updated to use Weather Underground and Flickr APIs.

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,44 @@
+var weatherText = "";
+
 var app = new Vue ({
     el: '#app',
     data: {
-        location: 'Los Angeles, CA',
-        temperature: '75 degrees',
-        conditions: 'cloudy',
+        results: []
+    },
+    mounted() {
+        axios.get("https://api.wunderground.com/api/f7f738f8dde51103/conditions/q/CA/Los_Angeles.json")
+        .then(response => {this.results = response.data.current_observation});
+    },
+    updated: function() {
+        weatherText = this.results.weather;
+        weatherText = parseWeatherText(weatherText);
+        callGetImageURL();
     }
 });
 
-//how to get image url from Flickr API results
-//https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-//'url('+ '\'https://farm.' + results[0].farm-id + '.staticflickr.com/' + results[0].server-id + '/' + results[0].id + '_' + results[0].secret + '.jpg' + ')' }
+function getImageURL() {
+    //var flickrImgArr;
+    return axios.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=18391479f53d149ee9f33d676616d003&tags=weather+" + weatherText + "&per_page=5&format=json&nojsoncallback=1")
+    .then( response => {
+        //flickrImgArr = response.data.photos.photo;
+        this.response = response.data.photos.photo;
+        return this.response[0];
+        //return bgImageURL;
+    });
+}
+
+function callGetImageURL() {
+getImageURL()
+.then(data => {
+    var bgImageURL;
+    bgImageURL = 'https://farm' + data.farm + '.staticflickr.com/' + data.server + '/' + data.id + '_' + data.secret + '_b.jpg';
+    document.getElementById('bgImage').style.backgroundImage = "url(" + bgImageURL + ')';
+});
+}
+
+function parseWeatherText(txt) {
+    if (txt.lastIndexOf(" ") > -1) {
+        txt = txt.replace(" ", "+");
+    }
+    return txt;
+}
