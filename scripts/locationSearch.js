94f679ca7efa78a9c6765ddde3d2c0b5dfbd453b
David Horton <d.r.horton@outlook.com>
2017-10-13 19:10:01 +0000

    Added Weather Underground Autocomplete Api call and search bar functionality

---
+++ scripts/locationSearch.js
@@ -0,0 +1,30 @@
+//Return real-time search results from Weatherground autocomplete API
+//Get Weather Underground JSON for list of locations to search on.
+
+function doSearch(text) {
+    var request = new XMLHttpRequest();
+    var queryURL = 'https://cors-anywhere.herokuapp.com/http://autocomplete.wunderground.com/aq?query=' + text
+    
+    //Get datalist and input elements
+    var dataList = document.getElementById('locations');
+    var inputSearch = document.getElementById('txtSearch');
+
+    request.onreadystatechange = function(response) {
+        
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                //Parse the JSON
+                var jsonLocations = JSON.parse(request.responseText);
+
+                //Loop over array and populate datalist
+                jsonLocations.RESULTS.forEach(function(item) {
+                    var option = document.createElement('option');
+                    option.value = item.name;
+                    dataList.appendChild(option);
+                });
+            }
+        }
+    };
+    request.open('GET', queryURL, true);
+    request.send();
+}
